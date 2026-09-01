4dbe0a7b7f0e1d4419ae9f81ddbef72ebc466651
tundra-node <117379918+tundra-node@users.noreply.github.com>
2026-08-31 21:49:41 -0400

    mini2: fix fileSystems fsType typo (fileSystem -> fsType)

--- hosts/mini/2/configuration.nix
+++ hosts/mini/2/configuration.nix
@@ -289,7 +289,7 @@
 
   # NAS — 2TB STORAGE on mini1 (moved from mini2)
   fileSystems."/mnt/storage" = {
-    fileSystem = "nfs4";
+    fsType = "nfs4";
     options = [ "nofail" "x-systemd.automount" "noatime" "soft" "timeo=100" ];
     device = "100.99.239.80:/mnt/storage";
   };
